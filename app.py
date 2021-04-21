fdf6a831d92d97004b0ee5fba592d67ac72ee939
Cankun Wang <flykun0620@gmail.com>
2021-04-20 21:05:37 -0400

    init dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     return 'Hello, world'
+
+
+@app.route('/gpu')
+def print_():
+    return os.system("nvidia-smi")
 
 
 if __name__ == '__main__':
