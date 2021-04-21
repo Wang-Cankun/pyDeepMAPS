ab0ece2ce7f486191bc5c29dbf4e485a454b6432
Cankun Wang <flykun0620@gmail.com>
2021-04-21 18:39:52 -0400

    Update redis

--- Python-client.Dockerfile
+++ Python-client.Dockerfile
@@ -1,5 +1,5 @@
 # runtime:2.3GB, devel: 5.27GB
-FROM wangcankun100/deepmaps-python-api-base
+FROM wangcankun100/deepmaps-python-base
 LABEL maintainer="Cankun Wang <cankun.wang@osumc.edu>"
 
 ENV FLASK_APP=app.py
@@ -8,6 +8,7 @@ ENV FLASK_RUN_PORT=5000
 
 EXPOSE 5000
 COPY . .
+RUN mkdir /data
 
 ENTRYPOINT ["/tini", "--"]
 CMD ["python","-m", "flask", "run"]
--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 from flask import Flask
 import os
+import time
+import redis
+import glob
 
 app = Flask(__name__)
+cache = redis.Redis(host='10.94.2.224', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.route('/')
-def hello_whale():
-    return 'Hello, world'
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+
+@app.route('/data')
+def list_data():
+    return glob.glob("/data")
 
 
 @app.route('/gpu')
-def print_():
+def print_gpu():
     return os.system("nvidia-smi")
 
 
